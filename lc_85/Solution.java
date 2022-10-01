016ff8be76b4f7ba3132b5c2a7791811f6ba3d14
Mason130 <78917758+Mason130@users.noreply.github.com>
2022-09-30 23:13:04 -0400

    update

---
+++ lc_85/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int[][] dp = new int[r+1][c+1];
+        
+        for(int i=1; i<r; i++){
+            for(int j=1; j<c; j++){
+                
+            }
+        }
+        
+        return 0;
+    }
+}
